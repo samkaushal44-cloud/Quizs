d2bb04821efc217512ec1f7de09907879f8f1400
samkaushal44-cloud <samkaushal44@gmail.com>
2026-01-01 16:24:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,87 +1,60 @@
-let questions=[],index=0,coins=0,timer,lock=false;
+const questions = [
+  {q:"India capital?", o:["Delhi","Mumbai","Kolkata","Chennai"], a:0},
+  {q:"2 + 2 = ?", o:["3","4","5","6"], a:1},
+  {q:"Sun rises from?", o:["North","South","East","West"], a:2},
+  {q:"HTML use for?", o:["Design","Structure","DB","Server"], a:1},
+  {q:"CSS full form?", o:["Style","Sheet","Cascading Style Sheets","Code"], a:2}
+];
 
-const qEl=document.getElementById("question");
-const oEl=document.getElementById("options");
-const tEl=document.getElementById("timer");
-const pBar=document.getElementById("progressBar");
-const cEl=document.getElementById("coins");
-const withdrawBox=document.getElementById("withdraw");
-const finalCoins=document.getElementById("finalCoins");
+let index = 0;
+let coins = 0;
 
-function decodeHTML(str){
-  const txt=document.createElement("textarea");
-  txt.innerHTML=str;
-  return txt.value;
-}
-
-async function loadAPI(){
-  const res=await fetch("https://opentdb.com/api.php?amount=10&type=multiple");
-  const data=await res.json();
-  questions=data.results.map(q=>{
-    const opts=[...q.incorrect_answers,q.correct_answer].sort();
-    return{
-      q:decodeHTML(q.question),
-      a:opts.map(o=>decodeHTML(o)),
-      c:opts.indexOf(q.correct_answer)
-    };
-  });
-  loadQuestion();
-}
-
-function startTimer(){
-  clearInterval(timer);
-  let t=15;
-  tEl.innerText=t;
-  timer=setInterval(()=>{
-    t--;
-    tEl.innerText=t;
-    if(t<=0){
-      clearInterval(timer);
-      nextQuestion();
-    }
-  },1000);
-}
+const qBox = document.getElementById("question");
+const optBox = document.getElementById("options");
+const nextBtn = document.getElementById("nextBtn");
+const coinBox = document.getElementById("coins");
 
 function loadQuestion(){
-  lock=false;
-  if(index>=questions.length){
-    qEl.style.display="none";
-    oEl.style.display="none";
-    withdrawBox.style.display="block";
-    finalCoins.innerText=coins;
-    return;
+  const q = questions[index];
+  qBox.innerText = q.q;
+  optBox.innerHTML = "";
+  nextBtn.style.display = "none";
+
+  q.o.forEach((text,i)=>{
+    const div = document.createElement("div");
+    div.className = "option";
+    div.innerText = text;
+    div.onclick = ()=>checkAnswer(div,i);
+    optBox.appendChild(div);
+  });
+}
+
+function checkAnswer(el,i){
+  const q = questions[index];
+  const options = document.querySelectorAll(".option");
+  options.forEach(o=>o.onclick=null);
+
+  if(i === q.a){
+    el.classList.add("correct");
+    coins += 10;
+  }else{
+    el.classList.add("wrong");
+    options[q.a].classList.add("correct");
   }
 
-  const q=questions[index];
-  qEl.innerText=q.q;
-  oEl.innerHTML="";
-  pBar.style.width=((index+1)/questions.length*100)+"%";
-
-  q.a.forEach((opt,i)=>{
-    const d=document.createElement("div");
-    d.className="option";
-    d.innerText=opt;
-    d.onclick=()=>{
-      if(lock) return;
-      lock=true;
-      if(i===q.c){
-        d.classList.add("correct");
-        coins+=10;
-        cEl.innerText=coins;
-      }else{
-        d.classList.add("wrong");
-      }
-      setTimeout(nextQuestion,800);
-    };
-    oEl.appendChild(d);
-  });
-
-  startTimer();
+  coinBox.innerText = coins;
+  nextBtn.style.display = "block";
 }
 
-function nextQuestion(){
+nextBtn.onclick = ()=>{
   index++;
+  if(index >= questions.length){
+    alert("Quiz Finished! Coins: " + coins);
+    index = 0;
+    coins = 0;
+    coinBox.innerText = coins;
+  }
   loadQuestion();
-}
+};
 
-loadAPI();
+loadQuestion();
